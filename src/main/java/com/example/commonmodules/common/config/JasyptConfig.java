55001d38db851f19773611fe0ed6fc8c982cff66
이상엽 <infightersy@naver.com>
2025-07-01 16:28:24 +0900

    불 필요 폴더 삭제

--- src/main/java/com/example/commonmodules/common/config/JasyptConfig.java
+++ src/main/java/com/example/commonmodules/common/config/JasyptConfig.java
@@ -1,23 +1,46 @@
 package com.example.commonmodules.common.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
+@Slf4j
+@RequiredArgsConstructor
 public class JasyptConfig {
-    // vm 옵션으로 전달받은 개인키
-    @Value("${jasypt.password}")
-    private String jasyptPassword;
 
+    private final Environment environment;
     // 전달받은 개인키 기반 암복호 객체를 빈으로 등록
     @Bean(name="jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
-        StringEncryptor encryptor = getEncryptor(jasyptPassword);
+//        String password = System.getProperty("jasypt.password");
+        String password = environment.getProperty("jasypt.password");
+
+        log.info("✅ jasypt.password from Environment => {}", password);
+
+        if (password == null || password.isBlank()) {
+            throw new IllegalStateException("jasypt.password system property not set!");
+        }
+//        return getEncryptor(password);
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(password);
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
         return encryptor;
+
     }
 
     /**
@@ -25,19 +48,19 @@ public class JasyptConfig {
      * @param key
      * @return
      */
-    private StringEncryptor getEncryptor(String key) {
-        // Config
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(key);                    // 암호화 키
-        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");    // 암호화 알고리즘
-        config.setKeyObtentionIterations("1000");   // 반복할 해싱 횟수
-        config.setPoolSize("1");                    // 인스턴스 pool
-        config.setProviderName("SunJCE");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");    // salt 생성 클래스
-        config.setStringOutputType("base64");       // 인코딩 방식
-
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        encryptor.setConfig(config);
-        return encryptor;
-    }
+//    private StringEncryptor getEncryptor(String key) {
+//        // Config
+//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+//        config.setPassword(key);                    // 암호화 키
+//        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");    // 암호화 알고리즘
+//        config.setKeyObtentionIterations("1000");   // 반복할 해싱 횟수
+//        config.setPoolSize("1");                    // 인스턴스 pool
+//        config.setProviderName("SunJCE");
+//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");    // salt 생성 클래스
+//        config.setStringOutputType("base64");       // 인코딩 방식
+//
+//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+//        encryptor.setConfig(config);
+//        return encryptor;
+//    }
 }
